790f4e788d5aef7f61780e64d7e371fdfcf78def
dextero <mradomsk@student.agh.edu.pl>
2013-02-02 11:54:10 +0100

    paczka

--- data/color_text.fs
+++
@@ -1,9 +0,0 @@
-varying vec2 vTexcoord;
-
-uniform sampler2D uTexture0;
-
-void main()
-{
-    vec4 col = texture2D(uTexture0, vTexcoord);
-    gl_FragColor = col + vec4(0.5, 0.5, 0.5, 1.0);
-}
--- data/texture.fs
+++
@@ -1,8 +0,0 @@
-varying vec2 vTexcoord;
-
-uniform sampler2D uTexture0;
-
-void main()
-{
-    gl_FragColor = texture2D(uTexture0, vTexcoord);
-}
