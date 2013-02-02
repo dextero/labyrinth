9f5c30c1ed28e24ffcea874238488cb875820554
dextero <mradomsk@student.agh.edu.pl>
2013-02-02 22:15:08 +0100

    update Sat, 02 Feb, 2013 22:15:07

--- data/diffuse.fs
+++ data/diffuse.fs
@@ -11,8 +11,8 @@ void main()
     vec3 diffuse = vColor * clamp(dot(vNormal, -lightDir), 0.0, 1.0);
 
     vec3 viewDir = normalize(-vPosition);
-    vec3 half = normalize(lightDir + viewDir);
-    float intensity = pow(clamp(dot(vNormal, half), 0.0, 1.0), 16.0);
+    vec3 halfDir = normalize(lightDir + viewDir);
+    float intensity = pow(clamp(dot(vNormal, halfDir), 0.0, 1.0), 16.0);
     vec3 specular = vec3(1.0) * intensity;
 
     vec3 color = clamp(ambient
