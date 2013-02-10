5db192fd7c10102498578e914c7ada066548eee2
dextero <mradomsk@student.agh.edu.pl>
2013-02-11 00:10:55 +0100

    update Mon, 11 Feb, 2013 00:10:54

--- data/color_text.fs
+++ data/color_text.fs
@@ -4,6 +4,5 @@ uniform sampler2D uTexture0;
 
 void main()
 {
-    vec4 col = texture2D(uTexture0, vTexcoord);
-    gl_FragColor = col + vec4(0.5, 0.5, 0.5, 1.0);
+    gl_FragColor = texture2D(uTexture0, vTexcoord) * 10.0;
 }
